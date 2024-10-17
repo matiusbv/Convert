7fc2f39f2519ca523d46827f0f691ef3aadaf99e
Marcio <matiusbv@gmail.com>
2024-10-17 15:22:08 -0300

    4,0 Identificando a moeda

--- scripts.js
+++ scripts.js
@@ -1,6 +1,36 @@
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+//Obtendo os elementos do formulário.
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 //Manipulando o input amout para receber somente números
 amount.addEventListener("input", () => {
   console.log(amount.value)
 })
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+ switch(currency.value) {
+  case "USD":
+    convertCurrency(amount.value, USD, "US$")
+    break
+  case "EUR":
+    convertCurrency(amount.value, EUR, "€")
+    break
+  case "GBP":
+    convertCurrency(amount.value, GBP, "£")
+
+ }
+
+
+}
+
+//Função para converter a moeda;
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol)
+}
